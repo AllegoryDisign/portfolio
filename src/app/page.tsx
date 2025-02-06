3a9e6899e0964b6a86b88c73bad4a74d58135606
AllegoryDisign <gavrilovich.ali@gmail.com>
2025-02-06 22:29:10 +0300

    +

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -169,7 +169,6 @@ export default function Home() {
                       sizes="50vw"
                       unoptimized
                       loading="lazy"
-                      unoptimized
                     />
                   </div>
                 </div>
